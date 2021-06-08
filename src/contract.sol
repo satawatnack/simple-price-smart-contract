7e05fb0bb7c265ce84b7f2f7d21c624468a10b55
satawatnack <s.nack1919@gmail.com>
2021-06-08 16:50:30 +0700

    add Coin struct

--- src/contract.sol
+++ src/contract.sol
@@ -1,19 +1,21 @@
 pragma solidity ^0.8.4;
 
 contract CoinContract {
-    mapping(string => uint) prices;
-     
-    constructor(string memory name, uint256 _price) {
-        prices[name] = _price; 
+     struct Coin {
+        uint rid;
+        uint price;
     }
-    
+    mapping(string => Coin) coins;
+     
     // function for get price
-    function GetPriceByName(string memory name) public view returns(uint256) {
-        return prices[name];
+    function GetPriceByName(string memory name) public view returns(uint, uint) {
+        return (coins[name].rid, coins[name].price);
     }
     
     // function for set price
-    function SePriceByName(string calldata name, uint256 _price) public{
-        prices[name] = _price;
+    function SetPriceByName(uint _rid, string memory name, uint _price) public {
+        Coin storage coin = coins[name];
+        coin.price = _price;
+        coin.rid = _rid;
     }
 }
